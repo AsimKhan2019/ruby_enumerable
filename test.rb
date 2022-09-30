73f2a66c06fefff485d6e7467838c42e0076d277
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-09-30 18:33:32 +0500

    Fixed linter errors

--- test.rb
+++ test.rb
@@ -4,9 +4,8 @@ list = [5, 7, 8, 9]
 
 list.all? { |e| e < 10 }
 list.all? { |e| e > 10 }
+binding.pry
 list.any? { |e| e == 2 }
 list.any? { |e| e == 5 }
 list.filter(&:even?)
 list.filter(&:odd?)
-
-binding.pry
